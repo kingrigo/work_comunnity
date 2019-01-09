2c09d8729d0c26eabc62e8029fce467cd0a43f95
Rodrigo <rorikstark@gmail.com>
2019-01-09 02:39:00 -0300

    Finished backend

--- routes/api/post.js
+++ routes/api/post.js
@@ -23,9 +23,12 @@ router.post(
       avatar: req.body.name,
       user: req.user.id
     })
-    newPost.save().then(post => {
-      res.json(post)
-    })
+    newPost
+      .save()
+      .then(post => {
+        res.json(post)
+      })
+      .catch(err => res.status(404).json(err))
   }
 )
 // Edit post
@@ -52,6 +55,7 @@ router.delete(
       .catch(err => res.json(err))
   }
 )
+
 // Get all posts
 router.get('/', (req, res) => {
   Post.find()
@@ -66,4 +70,119 @@ router.get('/:post_id', (req, res) => {
     .then(post => res.json(post))
     .catch(err => res.status(404).json(err))
 })
+
+// @route POST api/posts/like/:id
+// @desc Like a post
+// @access Private
+router.post(
+  '/like/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .lenght > 0
+          ) {
+            return res
+              .status(400)
+              .json({ alreadyLiked: 'User already liked this post' })
+          }
+          post.likes.unshift({ user: req.user.id })
+          post.save().then(post => res.json(post))
+        })
+        .catch(err => res.status(404).json(err))
+    })
+  }
+)
+
+// @route POST api/posts/unlike/:id
+// @desc Unlike a post
+// @access Private
+router.post(
+  '/unlike/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .lenght === 0
+          ) {
+            return res
+              .status(400)
+              .json({ alreadyLiked: 'You havent liked this post yet' })
+          }
+          const removeIndex = post.likes
+            .map(item => item.user.toString())
+            .indexOf(req.user.id)
+
+          post.likes.splice(removeIndex, 1)
+          post.save().then(post => res.json(post))
+        })
+        .catch(err => res.status(404).json(err))
+    })
+  }
+)
+
+// @route POST api/posts/comment/:id
+// @desc Add comment in post
+// @access Private
+router.post(
+  '/comment/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body)
+    if (!isValid) {
+      return res.status(400).json(errors)
+    }
+    Post.findById(req.params.id).then(post => {
+      const newComment = {
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.avatar,
+        user: req.user.id
+      }
+
+      post.comments.unshift(newComment)
+      post
+        .save()
+        .then(post => res.json(post))
+        .catch(err => res.status(404).json(err))
+    })
+  }
+)
+
+// @route DELETE api/posts/comment/:id/:comment_id
+// @desc Delete comment in post
+// @access Private
+router.delete(
+  '/comment/:id/:comment_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id).then(post => {
+      if (
+        post.comments.filter(
+          comment => comment._id.toString() === req.params.comment_id
+        ).lenght === 0
+      ) {
+        return res
+          .status(404)
+          .json({ commentNotExists: 'Comment does not exists' })
+      }
+      const removeIndex = post.comments
+        .map(item => item._id.toString())
+        .indexOf(req.params.comment_id)
+
+      post.comments.splice(removeIndex, 1)
+      post
+        .save()
+        .then(post => res.json(post))
+        .catch(err => res.status(404).json(err))
+    })
+  }
+)
+
 module.exports = router
